e1b7f5f47a233ae2a368a9d8b47d7192412fe057
Nitish <1rn19cs092.nitish.pkv@gmail.com>
2024-04-21 14:03:31 +0530

    Final Commit

--- main.py
+++ main.py
@@ -54,7 +54,9 @@ class Post(db.Model):
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    #posts = Post.query.all() -> To query all pages
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
     return render_template("home.html", posts=posts, user=current_user)
 
 
@@ -162,6 +164,15 @@ def delete_post(post_id):
         db.session.commit()
         flash("Post deleted successfully", 'success')
     return redirect(url_for('home'))
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    #posts = Post.query.all() -> To query all pages
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+    return render_template("user_posts.html", posts=posts, user=current_user)
 
 
 if __name__ == "__main__":
